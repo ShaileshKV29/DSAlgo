b5cbc208fd3b29d57fc9d6fad5b849ae6af4b8a4
ShaileshKV29 <shailesh29062001@gmail.com>
2022-02-05 11:11:17 +0530

    String

---
+++ Strings/string.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    char name[10] = {'J', 'o', 'h', 'n'};
+    printf("%s\n", name);
+
+    char name2[10];
+    scanf("%s\n", name2);
+    // printf("%s\n", name2);
+    return 0;
+}
---
+++ Strings/string.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char name[10] = "John";
+    char name2[10] = {'J', 'o', 'h', 'n'};
+    // printf("%s\n", name);
+    // printf("%s\n", name2);
+
+    char name3[10];
+    scanf("%s", name3);
+    printf("%s", name3);
+
+    return 0;
+}
