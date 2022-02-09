37dad884d03ce185537b1ddc4059ad0e1b214287
ShaileshKV29 <shailesh29062001@gmail.com>
2022-02-09 13:15:52 +0530

    String Bits Operation | Cool

---
+++ Strings/finding-duplicates-using-bits.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    char A[] = "finding";
+    int H = 0, x = 0, and;
+
+    for(int i = 0; A[i] != '\0'; i++)
+    {
+        x = 1;
+        x = x << (A[i] - 97);
+        // printf("%d ", x & H);
+        // and = x & H;
+        // if(and > 0)
+        //     printf("%d ", and);
+        if((x & H) > 0)
+            printf("%c is Duplicate\n", A[i]);
+        else
+            H = x | H;
+    }
+    // printf("%d", H & 32);
+
+    return 0;
+}
---
+++ Strings/finding-duplicates-using-bits.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char A[] = "finding";
+    int H = 0, x = 0;
+
+    for(int i = 0; A[i] != '\0'; i++)
+    {
+        x = 1;
+        x = x << (A[i] - 97);
+        // printf("%d ", x & H);
+        // and = x & H;
+        // if(and > 0)
+        //     printf("%d ", and);
+        if((x & H) > 0)
+            printf("%c is Duplicate\n", A[i]);
+        else
+            H = x | H;
+    }
+    // printf("%d", H & 32);
+
+    return 0;
+}
