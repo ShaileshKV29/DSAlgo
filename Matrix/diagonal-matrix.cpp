1a9da7d2660aea3e34774c2ab94feb7b1b2a7cd5
ShaileshKV29 <shailesh29062001@gmail.com>
2022-02-11 22:21:37 +0530

    Diagonal Matrix

---
+++ Matrix/diagonal-matrix.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int matrix[3][3] = {{1,0,0}, {0,2,0}, {0,0,3}};
+
+    // 1D Array Representation of Diagonal Matrix
+    int A[3] = {1,2,3};
+
+    return 0;
+}
