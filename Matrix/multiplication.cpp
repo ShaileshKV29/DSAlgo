4aecdfea3dd002e3cc44ae99e014bb235169e0c8
ShaileshKV29 <shailesh29062001@gmail.com>
2022-02-14 22:30:03 +0530

    Matrix Multiplication

---
+++ Matrix/multiplication.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int A[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+    int B[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+    int C[3][3] = {0};
+
+    int i,j,m,n;
+    for(i = 0; i < 3; i++)
+    {
+        int sum = 0;
+        for(j = 0; j < 3; j++)
+        {
+            // sum += 
+        }
+    }
+
+
+
+    return 0;
+}
