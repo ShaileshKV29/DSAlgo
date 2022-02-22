9a1e942ed9476549b0484eae894a3f8449c56ca0
ShaileshKV29 <shailesh29062001@gmail.com>
2022-02-22 23:01:02 +0530

    Linked List Traversal

---
+++ Linked List/linked_list_traversal.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+
+int main()
+{
+    struct Node n1,n2,n3,n4,n5;
+    n1.data = 10;
+    n1.next = &n2;    
+    n2.data = 20;
+    n2.next = &n3;    
+    n3.data = 30;
+    n3.next = &n4;    
+    n4.data = 40;  
+    n4.next = &n5;  
+    n5.data = 50;
+    n5.next = nullptr;
+
+    struct Node *first = &n1;
+    struct Node *p = first;
+
+    while(p != nullptr)
+    {
+        cout << p << "  " << p->data << endl;
+        p = p->next;
+    }
+       
+
+    return 0;
+}
