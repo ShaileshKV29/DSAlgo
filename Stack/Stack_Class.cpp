5f352a18625321436c3b1d8ddc26b14b0c1ac803
ShaileshKV29 <shailesh29062001@gmail.com>
2022-03-25 23:31:51 +0530

    Constructor using Array

--- Stack/Stack_Class.cpp
+++ Stack/Stack_Class.cpp
@@ -14,14 +14,23 @@ class Stack
             this->s = new int[this->size];
             int Top = -1;
         }
+
+        Stack(int size, int A[])
+        {
+            this->size = size;
+            this->s = new int[this->size];
+            for(int i = 0; i < this->size; i++)
+            {
+                s[i] = A[i];
+            }
+            this->Top = this->size - 1;
+        }
 };
 
 int main()
 {
-    int size;
-    printf("Enter the Size of Stack: ");
-    scanf("%d", &size);
-    Stack st(size);
+    int A[5] = {1,2,3,4,5};
+    Stack st(5, A);
 
     return 0;
 }
