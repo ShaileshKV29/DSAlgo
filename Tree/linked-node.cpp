3a20e5ffdf85a348d5640365b3b88ff432fc33fe
ShaileshKV29 <shailesh29062001@gmail.com>
2022-05-11 23:52:09 +0530

    Linked Node

---
+++ Tree/linked-node.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+class Node
+{
+    public:
+        Node *lchild;
+        int data;
+        Node *rchild;
+};
+
+class Tree
+{
+    public:
+
+};
+
+int main()
+{
+    
+
+    return 0;
+}
