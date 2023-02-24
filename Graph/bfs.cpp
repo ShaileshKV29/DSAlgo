8a8440a97e928274b0f87a9cf5b288397bad6c0a
ShaileshKV29 <shailesh29062001@gmail.com>
2023-02-24 15:58:43 +0530

    Geaphs

---
+++ Graph/bfs.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // unordered_map<int, vector<int>> adj_list;
+    // unordered_map<int, bool> visited;
+    // adj_list[0].push_back(3);
+
+    // adj_list[1].push_back(2);
+    // adj_list[1].push_back(3);
+    // adj_list[1].push_back(4);
+
+    // adj_list[2].push_back(1);
+
+    // adj_list[3].push_back(0);
+    // adj_list[3].push_back(1);
+
+    // adj_list[4].push_back(1);
+
+    // for(int i = 0;i < 5; i++)
+    // {
+    //     visited[i] = false;
+    // }
+
+    // queue<int> q;
+    // q.push(0);
+
+    // int front = 0;
+
+    map<int, bool> m;
+    for(int i = 0; i < 1; i++)
+    {
+        if(m[i] == true)
+            cout << m[i];
+        else    
+            cout << m[i];
+    }
+
+
+    return 0;
+}
